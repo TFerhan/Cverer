efc4cd3ec29cc896ad7f46138398ad473fa3fd3b
Taha Ferhan <132760925+TFerhan@users.noreply.github.com>
2024-02-06 23:07:47 +0100

    app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ import smtplib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'WlUrPgXjHOL9OquLGcQ96yfoZOyNLfCYFV05NUQN9DE3Fb4tUcWW02s4pYySzeDj'
-# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 convertapi.api_secret = 'tkHjgzt6QVQJjNSr'
 
-r = Redis.from_url('rediss://default:8aojn3wugN951YaiG4BglcuD0v31E5tTelkfrrxcDeyh8mFKkZFklVVL2qV7fXwe@dhj490.stackhero-network.com:6380')
+#r = Redis.from_url('<your-Redis-connection-url>')
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 q = Queue(connection=r)
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -58,19 +58,7 @@ def home():
             return resp
     return render_template('index.html', form=form)
 
-# @app.route('/analyze_results', methods=['GET', 'POST'])
-# def analyze_results():
-#     form = EmailForm()
-#     keyword = request.args.get('keyword')
-#     analyse = cache.get('analyse')
-#     print(analyse)
-#     dic_analyse = parse_string(analyse)
 
-#     print(dic_analyse)
-#     if form.validate_on_submit():
-#         email = form.email.data
-#         send_email(email, analyse)
-#     return render_template('analyse_results.html', details=dic_analyse, keyword=keyword, form = form)
 
 @app.route('/upload_pdf', methods=['GET', 'POST'])
 def upload_pdf():
@@ -88,42 +76,10 @@ def upload_pdf():
         q.enqueue(compare_data, resume_data, keyword, email)
         return render_template('sent.html')
     return render_template('resume.html', keyword=keyword, form = form, titles = titles)
-
-
-# @app.route('/results/', methods=['GET', 'POST'])
-# def results():
-#     form = EmailForm()
-#     details = cache.get('details')
-#     resume_data = cache.get('resume')
-#     keyword = request.args.get('keyword')
-#     if details and resume_data:
-#         start_time = time.time()
-#         result = compare_data(details, resume_data, keyword)
-#         print("--- %s seconds ---" % (time.time() - start_time))
-#         result_analysis = parse_string(result)
-#         cache.set('result', result_analysis, timeout=60*30)
-#         if form.validate_on_submit():
-#             email = form.email.data
-#             send_email(email, result_analysis)
-#         return render_template('results.html', result_analysis=result_analysis, form = form)
-#     else:
-#         abort(404)
     
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-# @app.route('/download_pdf', methods=['GET'])
-# def download_pdf():
-#     details = cache.get('analyse')
-#     with open('static/uploads/analyse.txt', 'w') as file:
-#         file.write(details)
-#     result = convertapi.convert('pdf', {'File': 'static/uploads/analyse.txt'})
-#     output_pdf_file = 'static/uploads/analyse.pdf'
-#     result.file.save(output_pdf_file)
-#     os.remove('static/uploads/analyse.txt')
-#     return send_file(output_pdf_file, as_attachment=True)
-
 
 @app.route('/lettre', methods=['GET', 'POST'])
 def lettre():
@@ -140,24 +96,6 @@ def lettre():
         return render_template('sent.html')
     return render_template('lettre.html', keyword=keyword, form = form, titles=titles)
 
-# @app.route('/lettre_results/', methods=['GET', 'POST'])
-# def lettre_results():
-#     form = EmailForm()
-#     details = cache.get('details')
-#     lettre_data = cache.get('lettre')
-#     keyword = request.args.get('keyword')
-
-#     if details and lettre_data:
-#         result = compare_data_let(details, lettre_data, keyword)
-#         result_analysis = parse_string(result)
-#         cache.set('lettre_results', result_analysis, timeout=60*30)
-#         if form.validate_on_submit():
-#             email = form.email.data
-#             send_email(email, result_analysis)
-#         return render_template('lettre_results.html', result_analysis=result_analysis, form = form)
-#     else:
-#         abort(404)
-    
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -187,29 +125,6 @@ def contact():
 
     return render_template('contact_us.html')
 
-# @app.route('/download_pdf_let', methods=['GET'])
-# def download_pdf_let():
-#     lettre_results = cache.get('lettre_results')
-#     with open('static/uploads/analyse.txt', 'w') as file:
-#         file.write(json.dumps(lettre_results))
-#     result = convertapi.convert('pdf', {'File': 'static/uploads/analyse.txt'})
-#     output_pdf_file = 'static/uploads/lettre_optim.pdf'
-#     result.file.save(output_pdf_file)
-#     os.remove('static/uploads/analyse.txt')
-#     return send_file(output_pdf_file, as_attachment=True)
-
-
-
-# @app.route('/download_pdf_res', methods=['GET', 'POST'])
-# def download_pdf_res():
-#     result_analysis = cache.get('result')
-#     with open('static/uploads/analyse.txt', 'w') as file:
-#         file.write(json.dumps(result_analysis))
-#     result = convertapi.convert('pdf', {'File': 'static/uploads/analyse.txt'})
-#     output_pdf_file = 'static/uploads/resume_optim.pdf'
-#     result.file.save(output_pdf_file)
-#     os.remove('static/uploads/analyse.txt')
-#     return send_file(output_pdf_file, as_attachment=True)
 
 if __name__ == '__main__':
 
